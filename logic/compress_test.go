9dcb789d88eba4dc2ae1a5d4a09f7a6be36e78b2
HotPotatoC <juandotulung@gmail.com>
2023-06-21 17:49:55 +0700

    test: updated compress input test case

--- logic/compress_test.go
+++ logic/compress_test.go
@@ -8,7 +8,47 @@ import (
 )
 
 func TestCompressDecompress(t *testing.T) {
-	original := []byte("Hello, World!")
+	original := []byte(`package logic
+
+	import (
+		"bytes"
+		"compress/zlib"
+		"io"
+	)
+
+	func Compress(b []byte) ([]byte, error) {
+		var buf bytes.Buffer
+
+		w := zlib.NewWriter(&buf)
+
+		_, err := w.Write(b)
+		if err != nil {
+			return nil, err
+		}
+
+		err = w.Close()
+		if err != nil {
+			return nil, err
+		}
+
+		return buf.Bytes(), nil
+	}
+
+	func Decompress(b []byte) ([]byte, error) {
+		buf := bytes.NewReader(b)
+		r, err := zlib.NewReader(buf)
+		if err != nil {
+			return []byte{}, err
+		}
+
+		res, err := io.ReadAll(r)
+		if err != nil {
+			return []byte{}, err
+		}
+
+		return res, nil
+	}
+	`)
 	compressed, err := logic.Compress(original)
 	if err != nil {
 		t.Error(err)
@@ -22,4 +62,7 @@ func TestCompressDecompress(t *testing.T) {
 	if !bytes.Equal(decompressed, original) {
 		t.Error("decompressed and original should be the same. got:", decompressed)
 	}
+
+	t.Log("compressed size:", len(compressed))
+	t.Log("original size:", len(original))
 }
